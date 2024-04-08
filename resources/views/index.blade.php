63e7ce48b92f6f06b2c682afd405f290b5e3d0aa
XopK <dmahmutov12@gmail.com>
2024-04-08 23:20:51 +0500

    header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,43 +15,22 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between p-3 mb-4" style="box-shadow: 0px 4px 20.899999618530273px rgba(0, 0, 0, 0.25)">
+        <div class="col-md-3 mb-2 mb-md-0">
             <a class="navbar-brand" href="#">PcGeek</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-            </div>
         </div>
-    </nav>
+
+        <div class="col-12 col-md-6 d-flex justify-content-center">
+            <form class="position-relative w-100" role="search">
+                <input type="search" class="form-control search focus-ring focus-ring-secondary" placeholder="Искать на PcGeek" aria-label="Search">
+                <img src="image/search.svg" alt="Search" class="position-absolute search-icon">
+            </form>
+        </div>
+
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-custom me-2">Войти</button>
+        </div>
+    </header>
 </body>
 
 </html>
